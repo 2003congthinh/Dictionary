009d59b964b2888f782c901ab9f45a95fe6f9a31
2003congthinh <96688734+2003congthinh@users.noreply.github.com>
2023-11-29 19:00:29 +0700

    change voice

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function pronounceWord(word) {
     var utterance = new SpeechSynthesisUtterance(word);
 
     // Use the default speech synthesis voice
-    utterance.voice = speechSynthesis.getVoices()[2];
+    utterance.voice = speechSynthesis.getVoices()[1];
 
     // Speak the word
     speechSynthesis.speak(utterance);
